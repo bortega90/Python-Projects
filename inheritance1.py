7e16541f996474910366c31aa07e2bd80eed257e
bortega90 <britnyortega@gmail.com>
2023-12-19 16:31:33 -0800

    update python repository

---
+++ inheritance1.py
@@ -0,0 +1,14 @@
+class Person:
+    name = 'Britny'
+    email = 'bort@gmail.com'
+    password ='123!abc'
+    account_id = 0
+
+class Student(Person):
+    department = 'Webdeveloper'
+    student_id = 23847483
+
+class Employee(Person):
+    base_pay = 25.00
+    weekly_hrs = 25
+    
