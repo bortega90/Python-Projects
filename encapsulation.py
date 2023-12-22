1432a3e85835f48ca5a0eb810e324f16b3c48298
bortega90 <britnyortega@gmail.com>
2023-12-21 16:29:28 -0800

    Update encapsulation.py
    add protected attribute

--- encapsulation.py
+++ encapsulation.py
@@ -1,6 +1,6 @@
 class Unchanged:
     def __init__(self):
-        self.__fixedVar = 88
+        self.__fixedVar = 88 #private(__) instance attribute
 
     def getFixed(self):
         print(self.__fixedVar)
@@ -14,3 +14,10 @@ obj.setFixed(44)
 obj.getFixed()
 
 
+class UnchangedProtected:
+    def __init__(self):
+        self._fixedVar = 5 #protected (_) instance attribute
+
+obj = UnchangedProtected()
+obj._fixedVar = 99
+print(obj._fixedVar)
