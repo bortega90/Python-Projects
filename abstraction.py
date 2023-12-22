c3342875dad4a9cae2c13075d6652eddfc61edc4
bortega90 <britnyortega@gmail.com>
2023-12-22 13:27:16 -0800

    Update abstraction.py

--- abstraction.py
+++ abstraction.py
@@ -5,22 +5,23 @@ from abc import ABC, abstractmethod
 
 
 class house(ABC):
+   
     def downPayment(self, amount):
-        print('Your down payment amount is: ', amount)
+        pass
 
-#pass b/c we are inputting amount when we call the function
+    @abstractmethod
     def cost(self, amount):
-        print('The house costs {}'.format(amount))
+        pass
 
 class mortage(house):
     #this will give us the montly amount... i wanted to input more variables
     #and simple formula to get the monthly amount but had a hard time trying
     #to divide values with variables so i gave up and just input amounts
-    def payment(self, amount):
+    def cost(self, amount):
         print('Your monthly payment will be ${}'.format(amount))
 
 
 obj = mortage()
-obj.cost("260,000")
+obj.cost("611.11")
 obj.downPayment("20,000")
-obj.payment("611.11")
+
