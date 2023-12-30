5e8f64623095fc60e040d878dd5acfa5e8b37bb8
bortega90 <147768691+bortega90@users.noreply.github.com>
2023-12-29 20:50:13 -0800

    Add files via upload

---
+++ DjangoUniversity/Classes/campusApp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create class called UniversityCampus with attributes.
+class UniversityCampus(models.Model):
+    campus_name = models.CharField(max_length=60, default="", blank=True, null=False)
+    state = models.CharField(max_length=2, default="", blank=True, null=False)
+    campus_ID = models.IntegerField(default="", blank=True, null=False)
+    # creates model manager
+    object = models.Manager()
+    # displays input values in the form of a string.
+    def __str__(self):
+        # returns the input value of the campus name, state, and campus id
+        # field as a tuple to display in the browser instead of the default times
+        display_location = "{0.campus_name}: {0.state}: {0.campus_ID}"
+        return display_location.format(self)
+
+    # displays University Campus
+    class Meta:
+        verbose_name_singular = 'University Campus'
+
