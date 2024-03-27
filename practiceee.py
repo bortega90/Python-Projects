9e01a0a8ec133dea2cf2275d24ddaa4fc3cd266b
bortega90 <britnyortega@gmail.com>
2024-03-26 19:58:59 -0700

    ChatBox Add
    added a simple OpenAI tkinter window utilitizing encapsulation

---
+++ practiceee.py
@@ -0,0 +1,17 @@
+
+message = {
+    3 : "love",
+    6 : "computers",
+    2 : "dogs",
+    4 : "cats",
+    1 : "I",
+    5 : "you"
+}
+def decode():
+    message_file = message[1,3,6]
+    print(message_file)
+
+
+
+
+    
